01117fcb884ef2f2bf98ba8ec3b7e16f1b618662
hq <1104735883@qq.com>
2019-01-21 16:03:56 +0800

    组件编写

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+import { StarsComponent } from './stars/stars.component';
+import { FootComponent } from './foot/foot.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ProductComponent,
+    StarsComponent,
+    FootComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
